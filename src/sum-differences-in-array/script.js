48611c7624b2876b65476602b1cef44832094f24
Rafael Pereira <orafaeldev@gmail.com>
2023-05-28 15:57:19 -0300

    feat: adicao de implementacao alternativa do desafio

--- src/sum-differences-in-array/script.js
+++ src/sum-differences-in-array/script.js
@@ -1,19 +1,21 @@
 const sumOfDifferences = (arr) => {
-  let results = []
-
   const sortedArray = arr.sort((a, b) => b - a)
 
-  for(let i = 0; i < sortedArray.length; i++) {
-    const nextNumber = sortedArray[i + 1]
+  // for(let i = 0; i < sortedArray.length; i++) {
+  //   const nextNumber = sortedArray[i + 1]
 
-    if(nextNumber === undefined) {
-      results.push(0)   
-    } else {
-      results.push(sortedArray[i] - nextNumber) 
-    } 
-  }
+  //   if(nextNumber === undefined) {
+  //     results.push(0)   
+  //   } else {
+  //     results.push(sortedArray[i] - nextNumber) 
+  //   } 
+  // }
 
-  return results.reduce((acc, item) => acc + item, 0)
+  // return results.reduce((acc, item) => acc + item, 0)
+
+  const result = sortedArray.map((currentItem, i, arr) => arr[i + 1] !== undefined ? currentItem - arr[i + 1] : 0)
+
+  return result.reduce((acc, item) => acc + item, 0)
 }
 
 module.exports = sumOfDifferences
